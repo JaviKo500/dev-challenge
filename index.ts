ec8260eb3df825b9bdb96668b5782c4da73dc749
JaViKo500 <javikogutierrez64@gmail.com>
2025-11-07 22:54:38 -0500

    feat: add challenge draw christmas tree

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 // import { countGiftsLetter2 } from "./src/advent2021/countGiftsLetter_2";
-import { validateGiftsLetter3 } from "./src/advent2021/validateGiftsLetter_3";
+import { drawChristmasTree4 } from "./src/advent2021/drawChristmasTree_4";
+// import { validateGiftsLetter3 } from "./src/advent2021/validateGiftsLetter_3";
 // import { countSheep } from "./src/advent2021/countSheep_1";
 
 // countSheep();
 // countGiftsLetter2();
-validateGiftsLetter3();
+// validateGiftsLetter3();
+drawChristmasTree4();
