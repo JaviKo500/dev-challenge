18f75c2af79cb719968e3afe3b1dd45db9212ded
JaViKo500 <javikogutierrez64@gmail.com>
2025-11-09 08:24:22 -0500

    feat: add challenge sum pairs numbers

--- index.ts
+++ index.ts
@@ -2,10 +2,11 @@
 // import { drawChristmasTree4 } from "./src/advent2021/drawChristmasTree_4";
 // import { validateGiftsLetter3 } from "./src/advent2021/validateGiftsLetter_3";
 // import { countSheep } from "./src/advent2021/countSheep_1";
-import { countDaysToChristmas5 } from "./src/advent2021/countDaysToChristmas_5";
+// import { countDaysToChristmas5 } from "./src/advent2021/countDaysToChristmas_5";
+import { sumPairs6 } from "./src/advent2021/sumPairs_6";
 
 // countSheep();
 // countGiftsLetter2();
 // validateGiftsLetter3();
 // drawChristmasTree4();
-countDaysToChristmas5();
+sumPairs6();
